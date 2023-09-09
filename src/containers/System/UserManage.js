3d74e1830411f8b15fcb4019a27acde056e6cacb
quoc loc <loc842211@gmail.com>
2023-09-09 21:08:25 +0700

    desgin modal nguoi dung

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 // import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import {getAllUsers} from '../../services/userServive'
+import {getAllUsers} from '../../services/userServive';
+import ModalUser from './ModalUser';
 class UserManage extends Component {
 
     constructor (props){
         super(props)
         this.state={
-          arrUsers:[]
+          arrUsers:[],
+          isOpenModalUser:false,
 
         }
     }
@@ -21,22 +23,43 @@ class UserManage extends Component {
       })
      }
     }
+    handleAddNewUser = ()=>{
+      this.setState({
+        isOpenModalUser:true
+      })
+    }
+
+    toggeleUserModal =()=>{
+      this.setState({
+        isOpenModalUser:!this.state.isOpenModalUser
+      })
+    }
 /** life cycle
  * reun component
  * 1.run construct -> init state
  * 2.Did mount (set state)
- * 3.render
+ * 3.render  (re-render)
  * 
  * @returns 
  * 
  */
 
     render() {
-      console.log('check',this.state)
-      let arrUsers =this.state.arrUsers
+      let arrUsers =this.state.arrUsers;
+      //properties; nested
         return (
             <div className="users-container">
+              <ModalUser
+              isOpen={this.state.isOpenModalUser}
+              toggleFromParent={this.toggeleUserModal}
+              test={'abc'}
+              />
                 <div className='title text-center'>MANAGE USERS WITH QUOC LOC</div>
+                <div className='mx-1'>
+                   <button className='btn btn-primary px-3'
+                   onClick={()=>this.handleAddNewUser()}
+                   ><i className='fas fa-plus'></i>Add new users</button>
+                </div>
                 <div className='users-table mt-3 mx-1'>
                 <table id="customers">
   <tr>
@@ -52,7 +75,7 @@ class UserManage extends Component {
         return(
           <tr>
             <td>{item.email}</td>
-           <td>{item.firtName}</td>
+           <td>{item.firstName}</td>
             <td>{item.lastName}</td>
             <td>{item.address}</td>
             <td>
