bcba0e1a202232d23e882bf4986190a1519f372b
quoc loc <loc842211@gmail.com>
2023-09-21 14:45:22 +0700

    markdown anh

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -4,6 +4,22 @@ import { connect } from 'react-redux';
 import './TableManageUser.scss';
 import * as actions from "../../../store/actions"
 
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log('handleEditorChange', html, text);
+}
+
 
 class TableManageUser extends Component {
     constructor (props){
@@ -33,7 +49,11 @@ class TableManageUser extends Component {
        console.log('check state ',this.state.usersRedux)
       let arrUsers=this.state.usersRedux
         return (
+            <React.Fragment>
                 <table id="TableManageUser">
+
+                    
+
                 <tbody>
             <tr>
             <th>Email</th>
@@ -65,8 +85,10 @@ class TableManageUser extends Component {
    
   
  
-</table>
-               
+                </table>
+
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+            </React.Fragment>
         );
     }
 
