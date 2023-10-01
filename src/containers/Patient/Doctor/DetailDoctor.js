fdd28fd937aab7be17cbe497482246f0b14dd25c
quoc loc <loc842211@gmail.com>
2023-10-01 17:14:40 +0700

    chon lich va load du lieeu

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,6 +4,7 @@ import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss'
 import {getDetailInforDoctor} from '../../../services/userServive'
 import { LANGUAGES } from '../../../utils';
+import DoctorSchedule from './DoctorSchedule';
 class DetailDoctor extends Component {
   constructor(props){
     super(props);
@@ -66,7 +67,14 @@ class DetailDoctor extends Component {
                     </div>
                 </div>
                 <div className='schedule-doctor'>
+                    <div className='content-left'>
+                            <DoctorSchedule
+                            doctorIdFromParent={detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                            />
+                    </div>
+                    <div className='content-right'>
 
+                    </div>
                 </div>
                 <div className='detail-infor-doctor'>
                             {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
---
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import './DoctorSchedule.scss'
+import moment from 'moment/moment';
+import localization from 'moment/locale/vi'
+import {getScheduleDoctorByDate} from '../../../services/userServive'
+import { LANGUAGES } from '../../../utils';
+
+
+
+class DoctorSchedule extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+       allDays:[],
+    }
+  }
+  async componentDidMount(){
+   let {language} =this.props;
+
+   console.log('vi', moment(new Date()).format('dddd - DD/MM'))
+   console.log('en',moment(new Date()).locale('en').format('ddd - DD/MM'))
+    this.setArrDays(language)
+}
+
+setArrDays =(language)=>{
+    let allDays =[]
+    for (let i=0;i<7;i++){
+     let object = {};
+     if(language === LANGUAGES.VI){
+     object.label=moment(new Date()).add(i,'days').format('dddd - DD/MM')
+ 
+     }else{
+     object.label=moment(new Date()).add(i,'days').locale('en').format('ddd - DD/MM')
+ 
+     }
+     object.value=moment(new Date()).add(i,'days').startOf('day').valueOf()
+ 
+     allDays.push(object)
+    }
+    
+    this.setState({
+     allDays:allDays
+    })
+}
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    if(this.props.language !== prevProps.language){
+    this.setArrDays(this.props.language)
+   
+    }
+  }
+  handleOnchageSelect =async (event)=>{
+
+    if(this.props.doctorIdFromParent && this.props.doctorIdFromParent !== -1){
+        let doctorId=this.props.doctorIdFromParent;
+        let date=event.target.value
+        let res =await getScheduleDoctorByDate(doctorId,date)
+        
+    
+    }
+
+  }
+
+    render() {
+        let {allDays}=this.state
+        return (
+         <div className='doctor-schedule-container'>
+            <div className='all-schedule'>
+                <select onChange={(event)=>this.handleOnchageSelect(event)}>
+                        {allDays && allDays.length >0 
+                    && allDays.map((item,index) => {
+                        return (
+                            <option
+                             value={item.value}
+                              key={index}
+                              >
+                                {item.label}
+                              </option>
+                        )
+                    })}
+                    
+                </select>
+            </div>
+            <div className='all-available-time'>
+
+            </div>
+         </div>   
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language:state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DoctorSchedule);
