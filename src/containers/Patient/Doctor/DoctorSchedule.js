d49a3548034ec4e00c1b963cba074291931e7576
quoc loc <loc842211@gmail.com>
2023-10-06 16:43:17 +0700

    load data hien ra man hinh

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -7,12 +7,12 @@ import {getScheduleDoctorByDate} from '../../../services/userServive'
 import { LANGUAGES } from '../../../utils';
 
 
-
 class DoctorSchedule extends Component {
   constructor(props){
     super(props);
     this.state={
        allDays:[],
+       allAvalableTime:[]
     }
   }
   async componentDidMount(){
@@ -23,13 +23,21 @@ class DoctorSchedule extends Component {
     this.setArrDays(language)
 }
 
+capitalizeFirstLetter(string){
+  return string.charAt(0).toUpperCase() +string.slice(1)
+}
+
+
 setArrDays =(language)=>{
     let allDays =[]
     for (let i=0;i<7;i++){
      let object = {};
      if(language === LANGUAGES.VI){
-     object.label=moment(new Date()).add(i,'days').format('dddd - DD/MM')
- 
+     let lableVi=moment(new Date()).add(i,'days').format('dddd - DD/MM')
+      
+     object.label=this.capitalizeFirstLetter(lableVi)
+
+
      }else{
      object.label=moment(new Date()).add(i,'days').locale('en').format('ddd - DD/MM')
  
@@ -50,24 +58,31 @@ setArrDays =(language)=>{
    
     }
   }
-  handleOnchageSelect =async (event)=>{
+  handleOnchangeSelect =async (event)=>{
 
     if(this.props.doctorIdFromParent && this.props.doctorIdFromParent !== -1){
         let doctorId=this.props.doctorIdFromParent;
         let date=event.target.value
         let res =await getScheduleDoctorByDate(doctorId,date)
-        
+         
+        if(res && res.errCode ===0){
+          this.setState({
+            allAvalableTime: res.data ? res.data:[]
+          })
+        }
+
     
     }
 
   }
 
     render() {
-        let {allDays}=this.state
+        let {allDays,allAvalableTime}=this.state;
+        let{language} =this.props
         return (
          <div className='doctor-schedule-container'>
             <div className='all-schedule'>
-                <select onChange={(event)=>this.handleOnchageSelect(event)}>
+                <select onChange={(event)=>this.handleOnchangeSelect(event)}>
                         {allDays && allDays.length >0 
                     && allDays.map((item,index) => {
                         return (
@@ -83,6 +98,23 @@ setArrDays =(language)=>{
                 </select>
             </div>
             <div className='all-available-time'>
+            <div className='text-calendar'>
+                     <i className='fas fa-calendar-alt'> <span>Lịch Khám</span></i>
+                    </div>
+                    <div className='time-content'>
+                      {allAvalableTime && allAvalableTime.length >0 ?
+                      allAvalableTime.map((item,index)=>{
+                        let timeDisplay =language === LANGUAGES.VI ?
+                        item.timeTypeData.valueVi : item.timeTypeData.valueEn
+                        // let timeType
+                        return(
+                          <button key={index}>{timeDisplay}</button>
+                        )
+                      })
+                      :
+                      <div>Không có lịch hẹn trong thời gian này, vui lòng chọn thời gian khác</div>
+                      }
+                    </div>
 
             </div>
          </div>   
